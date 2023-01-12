8e28031ae3d50865c81c240d3bb82d6397c01846
coubfrmdanap <alshiekh9012@icloud.com>
2023-01-12 11:41:53 -0500

    Initial commit

---
+++ Labs/lab0/js/app.js
@@ -0,0 +1,16 @@
+
+function setup() {
+    createCanvas(1200, 768);
+
+    background( 41, 27, 112);
+
+    rect( 40, 50, 100, 20);
+    fill(199, 169, 72);
+    rect( 40, 50, 100, 20);
+
+}
+
+function draw() {
+    circle(mouse, mouseY, 20);
+
+}
