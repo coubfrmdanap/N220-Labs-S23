46e3a0ae76bf3ecdc0eafad6cf81488d0190e830
coubfrmdanap <alshiekh9012@icloud.com>
2023-01-19 21:47:22 -0500

    Personal Composition

--- Labs/lab0/js/app.js
+++ Labs/lab0/js/app.js
@@ -1,16 +1,17 @@
-
 function setup() {
-    createCanvas(1200, 768);
-
-    background( 41, 27, 112);
-
-    rect( 40, 50, 100, 20);
-    fill(199, 169, 72);
-    rect( 40, 50, 100, 20);
-
-}
-
-function draw() {
-    circle(mouse, mouseY, 20);
-
-}
+    createCanvas(1000, 1000);
+    x = width / 2;
+    y = height / 2;
+  }
+  
+  function draw() {
+    background(200);
+    fill(250, 25, 25);
+    stroke(3);
+    strokeWeight(5);
+    ellipse(x, y, 45, 45);
+    rect(x - 30, y - 30, 45, 45);
+    triangle(x, y - 30, x - 30, y + 30, x + 30, y + 30);
+    line(x - 25, y, x + 25, y);
+    arc(x, y, 50, 50, 0, PI);
+  }
